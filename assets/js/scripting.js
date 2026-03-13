7c7c31718761d251223926f94fd5a64b8be59e3f
ashraf <ashrafsadekas@outlook.com>
2026-03-13 11:59:06 +0200

    last

--- assets/js/scripting.js
+++ assets/js/scripting.js
@@ -1,3 +1,14 @@
 document.write("welcome")
 document.write("<br><br>welcome")
 document.write("<h3>welcome</h3>")
+document.write("<ol><li>ahmed</li><li>hany</li></ol>")
+console.log(123)
+console.log("123")
+document.getElementById("pr").innerText="text text text"
+document.getElementById('dv').innerHTML="<strong><u>hello</u></strong>"
+document.getElementById("inp").value = "10"
+alert("iam alert")
+confirm("iam confirm")
+prompt("iam prompt")
+
+document.createElement("h4")
